31f28ac618050cea770dbb91507678ab72e60b87
Renan <renanbezerraenes@icloud.com>
2025-03-06 12:10:14 +0100

    Add login page, work with strings/styles/color/themes

---
+++ app/src/main/java/co/renanbezerra/instagram/login/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package co.renanbezerra.instagram.login.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.renanbezerra.instagram.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
