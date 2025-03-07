abe3b905ea295a06e427ab9a16184d270b890a77
Renan <renanbezerraenes@icloud.com>
2025-03-07 13:18:36 +0100

    add view for register with fragments

---
+++ app/src/main/java/co/renanbezerra/instagram/register/view/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package co.renanbezerra.instagram.register.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import co.renanbezerra.instagram.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
