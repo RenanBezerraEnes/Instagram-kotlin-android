15b0cb776156ffbf7e6325eca2109cf645cb38f3
Renan <renanbezerraenes@icloud.com>
2025-03-09 12:11:21 +0100

    add the main activity, and the bottom navigation view

---
+++ app/src/main/java/co/renanbezerra/instagram/main/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package co.renanbezerra.instagram.main.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import co.renanbezerra.instagram.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
