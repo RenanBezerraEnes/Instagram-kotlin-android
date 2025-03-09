6938ef01a2e57d78a943209f18f1b8853e9cd82f
Renan <renanbezerraenes@icloud.com>
2025-03-09 11:50:51 +0100

    add custom dialog, that can be reuse accross the whole application

--- app/src/main/java/co/renanbezerra/instagram/register/view/RegisterPhotoFragment.kt
+++ app/src/main/java/co/renanbezerra/instagram/register/view/RegisterPhotoFragment.kt
@@ -1,11 +1,14 @@
 package co.renanbezerra.instagram.register.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import co.renanbezerra.instagram.R
+import co.renanbezerra.instagram.common.view.CustomDialog
 
 class RegisterPhotoFragment : Fragment() {
     override fun onCreateView(
@@ -14,5 +17,26 @@ class RegisterPhotoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_register_photo, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val customDialog = CustomDialog(requireContext())
+
+        customDialog.setTitle(R.string.define_photo_profile)
+        customDialog.addBottom(R.string.photo, R.string.gallery) {
+            when (it.id) {
+                R.string.photo -> {
+                    Log.i("Test", "photo")
+                }
+
+                R.string.gallery -> {
+                    Log.i("Test", "Gallery")
+                }
+            }
+        }
+
+        customDialog.show()
     }
 }
