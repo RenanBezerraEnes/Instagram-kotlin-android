6f515d6b195836f12da4786d59e6097c4ee005b4
Renan <renanbezerraenes@icloud.com>
2025-03-10 12:29:30 +0100

    add camera fragment, later one I will change it, right now it is just to validate the UI

---
+++ app/src/main/java/co/renanbezerra/instagram/camera/camera/FragmentCamera.kt
@@ -0,0 +1,25 @@
+package co.renanbezerra.instagram.camera.camera
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import co.renanbezerra.instagram.R
+
+class FragmentCamera : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+}
